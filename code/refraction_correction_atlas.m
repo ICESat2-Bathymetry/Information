a48c5bb283f80129ebf6f365ce44694c17c28bb3
parrishOSU <christopher.parrish@oregonstate.edu>
2023-01-09 19:15:09 -0800

    Update refraction_correction_atlas.m

--- code/refraction_correction_atlas.m
+++ code/refraction_correction_atlas.m
@@ -46,14 +46,15 @@ end
 %% Preprocessing step: compute the Earth curvature correction. This 
 % correction uses a mean radius of the Earth of 6371 km and a mean orbital
 % altitude of ICESat-2 of 496 km
-thetaEC = atan(496*tan(deg2rad(ref_elev))/6371);
+% commented out, because not currently used
+% thetaEC = atan(496*tan(deg2rad(ref_elev))/6371);
 
 %% Step 0: convert ref_elev, which is defined in ATL03 as the elevation of
 % the unit pointing vector in the local ENU frame in radiance measured from 
 % the E-N plane positive towards up to incidence angle, theta1.
 theta1 = pi/2 - abs(ref_elev) + thetaEC - deltaTheta;
 
-%% Step 1: Compute D = Wij – Zij, where D is the uncorrected depth
+%% Step 1: Compute D = Wij â€“ Zij, where D is the uncorrected depth
 D = W - Z;
 
 %% Step 2: 	Compute theta2 from Eq. 2, where theta2 is the angle of 
@@ -62,27 +63,27 @@ theta2 = asin(n1*sin(theta1)/n2);
 
 %% Step 3: Compute S from Eq. 4, where S is the uncorrected slant range to 
 % the uncorrected bottom return photon location
-S = D/cos(theta1);
+S = D./cos(theta1);
 
 %% Step 4: Compute R from Eq. 3, where R is the corrected slant range
-R = (S*n1)/n2;
+R = (S.*n1)./n2;
 
 %% Step 5: Compute P from Eq. 10, where P is the distance between the 
 % uncorrected and corrected photon return points in the Y-Z plane
-P = sqrt(R^2+S^2-2*R*S*cos(theta1-theta2)); 
+P = sqrt(R.^2+S.^2-2.*R.*S.*cos(theta1-theta2));
 
 %% Step 6: Compute beta from Eq. 11
-beta = (pi/2) - theta1 - asin(R*sin(theta1-theta2)/P);
+beta = (pi/2) - theta1 - asin(R.*sin(theta1-theta2)./P);
 
 %% Step 7: Compute DeltaY and DeltaZ from Eq. 12-13, where DeltaY and 
 % DeltaZ are the offsets in the cross-track and vertical directions, 
 % respectively
-DeltaY = P*cos(beta);
-DeltaZ = P*sin(beta);
+DeltaY = P.*cos(beta);
+DeltaZ = P.*sin(beta);
 
 %% Step 8: Compute the cross track offset in the mapping frame
-DeltaE = DeltaY*sin(ref_azimuth);
-DeltaN = DeltaY*cos(ref_azimuth);
+DeltaE = DeltaY.*sin(ref_azimuth);
+DeltaN = DeltaY.*cos(ref_azimuth);
 
 %% Step 9: add the corrections to the original coordinates
 E_new = E + DeltaE;
